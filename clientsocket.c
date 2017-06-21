a57b50e4d2ecd09fe6f9fa902f5e13a30a6735d6
Nathalie Kuhn <MissNathalieKuhn@web.de>
2017-06-21 11:48:53 +0200

    clientsocket überarbeitet

--- clientsocket.c
+++ clientsocket.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
 int main(int argc, char *argv[]) {
 	struct sockaddr_in server;
@@ -6,24 +10,31 @@ int main(int argc, char *argv[]) {
 	int sockfd, n;
 	char buffer[256];
 
+	/*if(argc != 2) {
+		fprinf(stderr, "usage: client <hostname>\n");
+		exit(2);
+	}*/
+
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
 	if (sockfd < 0) {
-      		perror("ERROR opening socket");
-      		exit(1);
+		perror("ERROR opening socket");
+		exit(1);
 	}
 
 	host_info = gethostbyname(argv[1]);
 
-	/*if (server == NULL) {
-    		fprintf(stderr,"ERROR, no such host\n");
-      		exit(0);
-   	}*/
+	if (server == NULL) {
+		fprintf(stderr,"ERROR, no such host\n");
+		exit(0);
+  }
 
 	bzero((char *) &serv_addr, sizeof(serv_addr));
-	//serv_addr.sin_family = AF_INET;
+	server.sin_family = AF_INET;
 	memcpy(&server.sin_addr. host_info->h.addr. host_info->h_length);
-	server.sin_port = htons(1337);
+	/* copies internet address to server address */
+	//bcopy(hp->h_addr, &server.sin_addr, hp->h_length);
+	server.sin_port = htons(1344);
 
 	/* Mit Server verbinden */
 	if(connect(sockfd, (struct sockadrr*) &server, sizeof(server)) < 0) {
@@ -54,3 +65,16 @@ int main(int argc, char *argv[]) {
 	printf("%s\n", buffer);
 	return 0;
 }
+	/*read input from stdin*/
+	/*while(run=read(0,buf,BUF_SIZE)) {
+		if(run<0) {
+			perror(“error reading from stdin”);
+			exit(1);
+		}*/
+ 	/* write buffer to stream socket */
+ 		/*if(write(sock,buf,run) < 0) {
+			perror(“writing on stream socket”);
+			exit(1);
+		}
+	}*/
+	/*close(sock);*/
